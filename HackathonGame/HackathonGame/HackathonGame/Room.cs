c09bf5219e523aa96460f9c24300bf96fe33180c
Adam <creative-pixels@hotmail.com>
2012-10-12 19:29:04 -0400

    Compiles :)

--- HackathonGame/HackathonGame/HackathonGame/Room.cs
+++ HackathonGame/HackathonGame/HackathonGame/Room.cs
@@ -10,13 +10,14 @@ namespace HackathonGame
     public class Room
     {
         public Vector2 Size;
-
         Player player;
+        List<GameObject> blocks;
 
         public Room(Vector2 Size)
         {
             this.Size = Size;
             this.player = new Player(Vector2.Zero, this);
+            this.blocks = new List<GameObject>();
         }
 
         public void Update()
